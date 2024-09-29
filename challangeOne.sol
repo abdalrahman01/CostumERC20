828fc4fc67e246374220b38ab64c52dbea2a2974
abdalrahman01 <abdalrahman.atieh@gmail.com>
2024-09-29 10:32:20 +0300

    added buy/sell/transfer fees

--- challangeOne.sol
+++ challangeOne.sol
@@ -7,31 +7,64 @@ contract AbdToken is ERC20 {
 
     // address sender; 
     // address reciever; 
+    address unsiswap;
     
     uint8 transferFee = 1; // 1 %
     uint8 buyingFee = 2; // 2 %
     uint8 sellingFee = 3; // 3 %
 
-    address owner;  
 
     constructor() ERC20("AbdToken", "ATKN") {
-        _mint(msg.sender, 1000);
-
-        owner = msg.sender;
+        _mint(msg.sender, 100 * 10 ** 18 );
+        unsiswap = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
     }
 
 
-    function send(address sender, address recipient, uint256 amount, uint8 fee) internal   {
-        
-        uint256 amountFee = (amount * fee) /100; 
-        
-        uint256 amountAfterFee = amount - amountFee;
-
-        super._transfer(sender, owner, amountFee);
-        super._transfer(sender, recipient, amountAfterFee);
+    function calculateFee(uint256 value, uint8 fee) internal pure returns (uint256) {
+        return (value * fee) / 100; 
     }
 
-    function transfer(address sender, address recipient, uint256 amount) external  {
-        send(sender, recipient, amount, transferFee);
+    function transfer(address to, uint256 value) public override returns (bool) {
+        address owner = _msgSender();
+        
+        uint256 feeValue; 
+        if (to == unsiswap) {
+            // sell
+            feeValue = calculateFee(value, sellingFee);
+        } else {
+
+            // transfer
+            feeValue = calculateFee(value, transferFee);
+        }
+
+        value -= feeValue;
+        _transfer(owner, owner, value);
+        _transfer(owner, to, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public override returns (bool) {
+        address owner = _msgSender();
+
+
+        uint256 feeValue; 
+        if (from == unsiswap) {
+
+            // buy
+            feeValue = calculateFee(value, buyingFee);
+        } else if (to == unsiswap) {
+
+            // sell
+            feeValue = calculateFee(value, sellingFee);
+        } else {
+
+            feeValue = calculateFee(value, sellingFee);
+        }
+
+
+        value -= feeValue;
+        _transfer(owner, owner, value);
+        _transfer(from, to, value);
+        return true;
     }
 }
